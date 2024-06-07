df546bab962b19ba0cf9d115fd441dfc8c16581c
sandip <sandeep47nepali@gmail.com>
2024-06-07 17:13:16 +0545

    access control

--- server.js
+++ server.js
@@ -4,11 +4,19 @@ const axios = require("axios");
 const cors = require("cors");
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: ["https://khalti-api-pvem.onrender.com", "http://localhost:3000"],
-  })
-);
+app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
 
 require("dotenv").config();
 
